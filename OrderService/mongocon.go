3dca64810883c20c9b6f3380fb3219628724f90d
Narsimha9 <narsimhulu.464@gmail.com>
2020-07-25 11:34:38 +0530

    order commit

--- OrderService/mongocon.go
+++ OrderService/mongocon.go
@@ -10,10 +10,10 @@ import (
 var db *mgo.Database
 
 func init() {
-	// host := os.Getenv("MONGO_HOST")
+	host := os.Getenv("MONGO_HOST")
 	// // dbName := os.Getenv("MONGO_DB_NAME")
 	dbName := "test"
-	// fmt.Println("conn info:", host, dbName)
+	fmt.Println("conn info:", host, dbName)
 	session, err := mgo.Dial("mongodb://localhost:27017")
 	if err != nil {
 		fmt.Println("session err:", err)
